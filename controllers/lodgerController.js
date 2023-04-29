b962ca76043d54e7d64e9d7c5cf3055aa25fef3e
Pawan Kalyan <pawankalyan1614@gmail.com>
2023-04-29 14:20:59 +0530

    roomrequest

--- controllers/lodgerController.js
+++ controllers/lodgerController.js
@@ -1,5 +1,6 @@
 const Lodger = require("../models/lodger");
 const PG = require("../models/PG");
+const Landlord = require("../models/landlord");
 
 const index_get = (req, res) => {
     const userID = req.user.id;
@@ -37,7 +38,61 @@ const index_post = (req, res) => {
     })
 }
 
+const pg_get = (req, res) => {
+    const pgID = req.params.id;
+    PG.findById(pgID)
+    .then((pg) => {
+        pgdata = pg;
+        Landlord.findById(pg.ownerId)
+        .then((landlord) => {
+            const userID = req.user.id;
+            Lodger.findById(userID)
+            .then((user) => {
+                const lodger_data = user;
+                res.render('lodgerpgdetails', {pg : pgdata, landlord : landlord, lodger : lodger_data});
+            })
+            .catch((err) => {
+                res.send({err : err});
+            })
+        })
+        .catch((err) => {
+            res.send({err : err});
+        })
+    })
+    .catch((err) => {
+        res.send({err : err});
+    });    
+}
+
+const roomrequest = (req, res) => {
+    const lodgername = req.body.lodgername;
+    const lodgeremail = req.body.lodgeremail;
+    const lodgerphone = req.body.lodgerphone;
+    const lodgerId = req.user.id;
+    const pgid = req.params.id;
+    console.log(pgid)
+    PG.findById(pgid)
+    .then((pg) => {
+        console.log(pg);
+        pg.RoomRequests.push({lodgername : lodgername, lodgeremail : lodgeremail, lodgerphone : lodgerphone, lodgerId : lodgerId});
+        pg.save()
+        .then((pg) => {
+            res.redirect('/lodgerdashboard/pg/' + pgid);
+        })
+        .catch((err) => {
+            res.send({err : err});
+        })
+    })
+}
+
+const savepg = (req, res) => {
+    const userId = req.user.id;
+    //to do
+}
+
 module.exports = {
     index_get,
     index_post,
+    pg_get,
+    roomrequest,
 }
--- routes/lodgerRoute.js
+++ routes/lodgerRoute.js
@@ -7,4 +7,8 @@ router.get('/', lodgerController.index_get);
 
 router.post('/', lodgerController.index_post);
 
+router.get('/pg/:id', lodgerController.pg_get);
+
+router.post('/pg/:id/requestroom', lodgerController.roomrequest);
+
 module.exports = router;
